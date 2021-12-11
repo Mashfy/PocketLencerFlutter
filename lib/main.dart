6bff79527312652b415802553b33a2cb8335c6b0
Mashfy <badrulalamsiddiuqe@gmail.com>
2021-12-12 03:09:33 +0600

    firebase set up done

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,16 @@ import 'package:pocket_lencer/blocs/cart/cart_bloc.dart';
 import 'package:pocket_lencer/blocs/wishlist/wishlist_bloc.dart';
 import 'package:pocket_lencer/config/app_router.dart';
 import 'package:pocket_lencer/screens/screens.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'config/theme.dart';
 
 // ignore_for_file: prefer_const_constructors
 // ignore_for_file: sized_box_for_whitespace
 // ignore_for_file: use_key_in_widget_constructors
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
